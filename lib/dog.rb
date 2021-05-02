c11ccf556e76d0f69efa31fa319dafa950a1e406
Jon <jbrundagejr@gmail.com>
2021-05-02 12:53:18 -0400

    commit tree b0fa64fbcf402ec76614658814290051de64ec4c

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,19 @@
-# Add your code here
+class Dog
+  
+  @@all = []
+  attr_reader :name
+  
+  def initialize(name="name")
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.print_all
+    puts @@all[name]
+  end
+
+end
